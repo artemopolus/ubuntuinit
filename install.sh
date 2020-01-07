ef0a1867e8d41b58c6a68b61ba15afa7ea071688
Artem Kuznecov <artem.o.kuznetcov@yandex.ru>
2020-01-07 13:45:31 +0300

    install script plus tmux

---
+++ install.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+sudo apt-get install cmake
+sudo apt-get install git
+sudo apt-get install gcc
+sudo apt-get install g++
+sudo apt-get install doxygen doxygen-latex
+I=`dpkg -s sublime-text | grep "Status" ` #проверяем состояние пакета (dpkg) и ищем в выводе его статус (grep)
+if [ -n "$I" ] #проверяем что нашли строку со статусом (что строка не пуста)
+then
+   echo "sublime-text installed" #выводим результат
+else
+   	echo "sublime-text not installed"
+   	wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+
+	sudo apt-get install apt-transport-https
+
+	echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+
+	sudo apt-get update
+
+	sudo apt-get install sublime-text
+fi
+
+#file space
+if [ -d workspace ]
+then
+	echo "Dir workspace exists"
+else
+	echo "workspace not exists. Create"
+	mkdir workspace
+fi
+
+ 
+
+I=`dpkg -s rsync | grep "Status" `
+if [ -n "$I" ] 
+then
+	echo "rsync installed" 
+else
+	sudo apt-get install rsync
+fi
+
+
+I=`dpkg -s python | grep "Status" `
+if [ -n "$I" ] 
+then
+	echo "python installed" 
+else
+	sudo apt-get install python python-pip
+fi
+
+I=`dpkg -s python3 | grep "Status" `
+if [ -n "$I" ] 
+then
+	echo "python3 installed" 
+else
+	sudo apt-get install python3
+fi
+
+I=`dpkg -s python3-pip | grep "Status" `
+if [ -n "$I" ] 
+then
+	echo "python3-pip installed" 
+else
+	sudo apt-get install python3-pip
+fi
+
+
+echo "Done!"
